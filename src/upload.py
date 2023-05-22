d2133140a78bbb5465564b2e51c0ba6bcdf81e87
andreakatzenberger <andrea.katzenberger01@gmail.com>
2023-05-22 15:57:42 +0200

    Brisanje sadrzaja Close #12

--- src/upload.py
+++ src/upload.py
@@ -10,15 +10,15 @@ dynamodb_client = boto3.client('dynamodb')
 
 
 def s3_create_bucket(bucket_name):
-    s3_client.create_bucket(Bucket = bucket_name,
-                         CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
+    s3_client.create_bucket(Bucket=bucket_name,
+                            CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
 
 
 def s3_upload_file(file_path, bucket_name, file_name):
     s3_client.upload_file(file_path, bucket_name, file_name)
 
 
-def dynamodb_create_table(table_name, pk_name, sk_name = None):
+def dynamodb_create_table(table_name, pk_name, sk_name=None):
     attribute_definitions = [
         {
             'AttributeName': pk_name,
@@ -47,15 +47,16 @@ def dynamodb_create_table(table_name, pk_name, sk_name = None):
         'WriteCapacityUnits': 3
     }
 
-    dynamodb_client.create_table(TableName = table_name, AttributeDefinitions = attribute_definitions, KeySchema = key_schema, ProvisionedThroughput = provisioned_throughput)
+    dynamodb_client.create_table(TableName=table_name, AttributeDefinitions=attribute_definitions, KeySchema=key_schema,
+                                 ProvisionedThroughput=provisioned_throughput)
 
 
 def dynamodb_insert_into_table(table_name, item):
     item_json = json_util.dumps(item, as_dict=True)
-    dynamodb_client.put_item(TableName = table_name, Item = item_json)
+    dynamodb_client.put_item(TableName=table_name, Item=item_json)
 
 
-def upload(name, file_path, file_name, description = '', tags=None):
+def upload(name, file_path, file_name, description='', tags=None):
     if tags is None:
         tags = []
     s3_upload_file(file_path, name, file_name)
@@ -91,11 +92,39 @@ def dynamodb_check_if_exists(table_name, key, value):
         return False
 
 
+def delete_file(username, filename):
+    try:
+        s3_client.delete_object(Bucket=username, Key=filename)
+        print("File deleted successfully from s3.")
+    except Exception as e:
+        print("Error deleting file from s3:", str(e))
+
+    try:
+        primary_key = {
+            'file_name': {
+                'S': filename
+            }
+        }
+        dynamodb_client.delete_item(
+            TableName=username,
+            Key=primary_key
+        )
+        print("Item deleted successfully from dynamodb.")
+    except Exception as e:
+        print("Error deleting item from dynamodb:", str(e))
+
+
+# Example usage
+bucket_name = 'proba-123-321'
+item_key = 'cloud_todo'
+delete_file(bucket_name, item_key)
+
+# "user-" +
 
 # s3_create_bucket('final-test-123')
 # dynamodb_create_table('users', 'username')
 
-# upload('final-test-123', 'C:/Users/Svetozar/Desktop/download.png', 'download', 'opis')
+upload('user-andrea01', 'C:/Users/andre/OneDrive/Desktop/VSEM.txt', 'vsem', 'opissss')
 
 # primary_key = {
 #     "file_name": {"S": "download"}
